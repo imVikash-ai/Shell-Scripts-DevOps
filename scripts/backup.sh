1647b46738c6fd8b071ced8d3ac9e75443569e37
Vikash Kumar <kumarvikash97716880@gmail.com>
2026-09-17 16:57:25 +0530

    file add

---
+++ scripts/backup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+<<info
+This shell ecripts will take periodic bacups
+
+eg.  ./backup.sh <source> <destination>
+info
+
+#src=/mnt/c/Programming/AWS_DEVOPS/Shell_Scripts_TWS/scripts
+src=$1
+dest=$2
+
+timestamp=$(date '+%Y-%m-%d')
+
+zip -r "$dest/backup-$timestamp.zip" $src > /dev/null
+
+echo "Backup Done"
